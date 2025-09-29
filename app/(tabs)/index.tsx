dff77b275d57f79410a60c69b220f5c04f743b19
nathanstream203 <nathanstream203@gmail.com>
2025-09-29 12:08:43 -0500

    home and bar icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Circle, Marker } from 'react-native-maps';
 import { Colors } from '../theme';
+import markerData from '../markers.json';
 
 
 export default function HomeScreen() {
@@ -32,32 +33,36 @@ const circleRadius = 5000;
 
             />
 
-              <Marker coordinate={{latitude: 44.878091740523985, longitude: -91.92843302663482}}>
-                  <View
-                  style={{
-                  backgroundColor: Colors.primary,
-                  borderRadius: 20,
-                  padding: 8,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  }}
+              {markerData.map((marker, index) => {
+                  const backgroundColor =
+                  marker.icon === 'beer-outline'
+                  ? Colors.primary // lighter color for houses
+                  : Colors.secondary;
+                  return (
+                  <Marker
+                      key={index}
+                      coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
                   >
-                  <Ionicons
-                  name="home-outline"
-                  size={20}   // smaller than circle
-                  color="white"
-                  style={{ marginTop: 1, marginLeft: 1 }} // adjust if needed
-                          />
-                  </View>
-              </Marker>
-              <Marker coordinate={{latitude: 44.87805290393327, longitude: -91.92962985786905}}/>
-              <Marker coordinate={{latitude: 44.87720510396106, longitude: -91.92994142428837}}/>
-              <Marker coordinate={{latitude: 44.87705427213555, longitude: -91.92993748257626}}/>
-              <Marker coordinate={{latitude:44.87639168428689, longitude: -91.92544797922613}}/>
-              <Marker coordinate={{latitude: 44.87663124956, longitude: -91.92667921682191}}/>
-              
-
-
+                      {marker.icon && (
+                          <View
+                              style={{
+                                  backgroundColor,
+                                  borderRadius: 20,
+                                  padding: 8,
+                                  justifyContent: 'center',
+                                  alignItems: 'center',
+                              }}
+                          >
+                              <Ionicons
+                                  name={marker.icon as any}
+                                  size={20}
+                                  color="white"
+                              />
+                          </View>
+                      )}
+                  </Marker>
+                  );
+              })}
           </MapView>
       </View>
   );
