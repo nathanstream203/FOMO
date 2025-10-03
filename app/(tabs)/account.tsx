af74d5fb796cee30f8b9c0a810f8d04daa17f05b
GabeSahlin <gabesahlin@gmail.com>
2025-10-03 11:26:07 -0500

    Revert "SIgnin, Signout, Signup"
    This reverts commit 9e7f749f51c2de836c27a15404717360f75ea47a.

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,6 +1,20 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { signOut } from 'firebase/auth';
+import { StyleSheet, Text, View } from 'react-native';
+import { auth } from '../firebaseConfig';
 
 export default function AccountScreen() {
+
+  const router = useRouter();
+
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+      router.replace('/signin');
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Account screen</Text>
