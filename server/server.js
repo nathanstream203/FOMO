4ea15964c05250b19b0f17689e2f4d7075d52633
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-10-08 11:40:02 -0500

    Backend boilerplate

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Default route
+app.get('/', (req,res) => {
+    res.json({'message': 'Hello from the backend!'});
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
