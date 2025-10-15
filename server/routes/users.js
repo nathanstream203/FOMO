04b8d8190c0aee4b18d89d15e318233528a3a642
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-10-15 10:27:12 -0500

    User table API

---
+++ server/routes/users.js
@@ -0,0 +1,17 @@
+import express from "express";
+import prisma from "../prisma.js";
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const users = await prisma.users.findMany();
+    res.json(users);
+});
+
+router.post('/', async (req, res) => {
+    const { role_id } = req.body;
+    const user = await prisma.user.create({ data: { user_id } });
+    res.status(201).json(user);
+});
+
+export default router;
