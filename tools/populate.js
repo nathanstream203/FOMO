e487a4e15103b6f946747efdd52f51c11fe6606a
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-10-20 11:35:14 -0500

    Added users

--- tools/populate.js
+++ tools/populate.js
@@ -22,11 +22,21 @@ async function populateBars() {
     })
 }
 
+async function populateUsers() {
+    const users = await prisma.users.createMany({
+        data: [
+            {firebase_id: '1', first_name: 'dummy', last_name: 'bob', birth_date: '2000-01-01T01:01:00Z', role_id: 1},
+            {firebase_id: '2', first_name: 'dummy2', last_name: 'bob2', birth_date: '2001-01-01T01:01:00Z', role_id: 2}
+        ]
+    })
+}
+
 console.log("Welcome to the population tool");
 console.log('\nPlease select an option below:');
 console.log('1. Roles');
-console.log('2. Bars');
-console.log('3. Run all');
+console.log('2. Users');
+console.log('3. Bars');
+console.log('4. Run all');
 let choice = -1
 while(true){
     choice = readlineSync.question('>');
@@ -42,12 +52,21 @@ if(choice == 1){
             console.error(e);
         });
 }else if(choice == 2){
-    populateBars()
+    populateUsers()
         .catch(e => {
             console.error(e);
         });
 }else if(choice == 3){
+    populateBars()
+        .catch(e => {
+            console.error(e);
+        });
+}else if(choice == 4){
     populateRoles()
+        .catch(e => {
+            console.error(e);
+        });
+    populateUsers()
         .catch(e => {
             console.error(e);
         });
