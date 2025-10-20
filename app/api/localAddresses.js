ab199cab2b288fdd39550c956b7390d60781f4fe
GabeSahlin <gabesahlin@gmail.com>
2025-10-20 11:12:45 -0500

    Base URL correction

---
+++ app/api/localAddresses.js
@@ -0,0 +1,17 @@
+// --- IP Hosting File for local database --
+// --- Update the BASE_URL to switch between different local servers ---
+
+// PLEASE update the BASE_URL below to match the local IP address of you device depending on where you are using it.
+// PLEASE make sure to add this file to your .gitignore to avoid pushing local IP addresses to remote repos.
+
+
+//const BASE_URL = 'http://localhost:5000'; 
+
+// --- Home ---
+//const BASE_URL = 'http://192.168.1.145:5000'; 
+
+// --- Jarvis SW (Classroom) ---
+const BASE_URL = 'http://10.200.120.231:5000';
+
+// --- Sorenson (TechDesk) --- 
+//BASE_URL = 'http://10.200.121.114:5000';
