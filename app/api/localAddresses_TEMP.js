c5a9724c2ff55e9110e955e24d233a8f456e9ab0
GabeSahlin <gabesahlin@gmail.com>
2025-10-20 11:36:42 -0500

    localhost addresses

--- app/api/localAddresses_TEMP.js
+++ app/api/localAddresses_TEMP.js
@@ -1,17 +1,12 @@
-// --- IP Hosting File for local database --
-// --- Update the BASE_URL to switch between different local servers ---
+/*
 
-// PLEASE update the BASE_URL below to match the local IP address of you device depending on where you are using it.
-// PLEASE make sure to add this file to your .gitignore to avoid pushing local IP addresses to remote repos.
+--- IP Hosting File for local database --
+--- Update the BASE_URL to switch between different local servers ---
 
+# DO NOT MODIFY THIS FILE!!!
+# Make a copy of this file called localAdresses.js inside the 'api' folder and modify the variables there
 
-//const BASE_URL = 'http://localhost:5000'; 
+change the 'localhost' to your local IP address for the node server to connect to the frontend
+*/
 
-// --- Home ---
-//const BASE_URL = 'http://192.168.1.145:5000'; 
-
-// --- Jarvis SW (Classroom) ---
-const BASE_URL = 'http://10.200.120.231:5000';
-
-// --- Sorenson (TechDesk) --- 
-//BASE_URL = 'http://10.200.121.114:5000';
+const BASE_URL = 'http://localhost:5000'; 
