b130e1fb1527749f880a2a32b0d84961dfdca314
Alex Berg <123572908+berga4439@users.noreply.github.com>
2025-10-20 11:46:53 -0500

    semantic changes

--- tools/populate.js
+++ tools/populate.js
@@ -5,7 +5,7 @@ import readlineSync from 'readline-sync';
 const prisma = new PrismaClient();
 
 async function populateRoles() {
-    const roleList = await prisma.roles.createMany({
+    await prisma.roles.createMany({
         data: [
             { name: 'Basic', description: 'Basic user role'},
             { name: 'Admin', description: 'Admin user role'}
@@ -14,7 +14,7 @@ async function populateRoles() {
 }
 
 async function populateBars() {
-    const bars = await prisma.bar.createMany({
+    await prisma.bar.createMany({
         data: [
             {name: 'Test Bar 1', address: '123 Main St', latitude: 44.876503, longitude: -91.926781},
             {name: 'Test Bar 2', address: '456 Main St', latitude: 44.876311, longitude: -91.929753}
@@ -23,7 +23,7 @@ async function populateBars() {
 }
 
 async function populateUsers() {
-    const users = await prisma.users.createMany({
+    await prisma.users.createMany({
         data: [
             {firebase_id: '1', first_name: 'dummy', last_name: 'bob', birth_date: '2000-01-01T01:01:00Z', role_id: 1},
             {firebase_id: '2', first_name: 'dummy2', last_name: 'bob2', birth_date: '2001-01-01T01:01:00Z', role_id: 2}
@@ -46,22 +46,22 @@ while(true){
     }
     console.log('Invalid Choice');
 }
-if(choice == 1){
+if(choice === 1){
     populateRoles()
         .catch(e => {
             console.error(e);
         });
-}else if(choice == 2){
+}else if(choice === 2){
     populateUsers()
         .catch(e => {
             console.error(e);
         });
-}else if(choice == 3){
+}else if(choice === 3){
     populateBars()
         .catch(e => {
             console.error(e);
         });
-}else if(choice == 4){
+}else if(choice === 4){
     populateRoles()
         .catch(e => {
             console.error(e);
