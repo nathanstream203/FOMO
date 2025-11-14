6014d5235604e5d3e25653a0f541e1d663945156
EthanKoehler <etho3104@gmail.com>
2025-11-14 11:40:16 -0600

    Frontend points display

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,8 +1,24 @@
 // app/(tabs)/_layout.tsx
 
+import { View, Text, StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import { Colors } from "../styles/colors";
+
+const HeaderPoints = () => {
+  const userPoints = 1234;
+  return (
+  <View style={styles.pointsBubble}>
+    <Ionicons
+    name="server"
+    size={16}
+    color={Colors.secondaryLight}
+    style={{ marginRight: 4}}
+    />
+    <Text style={styles.pointsText}>{userPoints}</Text>
+  </View>
+  );
+};
 
 export default function TabLayout() {
   return (
@@ -17,6 +33,7 @@ export default function TabLayout() {
         tabBarStyle: {
           backgroundColor: Colors.primary,
         },
+        headerRight: () => <HeaderPoints />,
       }}
     >
       <Tabs.Screen
@@ -61,3 +78,32 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+
+const styles = StyleSheet.create({
+  pointsBubble: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.primary,
+    borderWidth: 0.3,
+    borderColor: Colors.secondary,
+    borderRadius: 20,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    marginRight: 15,
+
+    shadowColor: Colors.secondary,
+    shadowOffset: { width: 0, height: 0},
+    shadowOpacity: 0.8,
+    shadowRadius: 6,
+    elevation: 10,
+  },
+  pointsText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "bold",
+    textShadowColor: Colors.secondaryLight,
+    textShadowOffset: { width: 0, height: 0},
+    textShadowRadius: 10,
+  }
+});
