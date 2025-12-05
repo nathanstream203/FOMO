b964fc1c43f38a7174bc6c5112f71e4bec47f052
Lucah Lucchesi <lucahlucchesi@gmail.com>
2025-12-05 10:59:26 -0600

    auth

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -13,6 +13,10 @@ export async function authenticateToken(req, res, next) {
         if(!header) { 
             console.log(header);
             return res.status(401).json({ error: "Not authorized" })};
+=======
+        const header = req.headers.authorization;
+        if(!header) { return res.status(401).json({ error: "Not authorized" })};
+>>>>>>> 7d83b44601b250d0caf4e859c01ad9320de52ad1
 
         const token = header.split(" ")[1];
         const decoded = verifyToken(token);
